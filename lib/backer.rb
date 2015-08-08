969eca7c74f100dd471f1d51bfedba7dca489b96
N K <konancys@gmail.com>
2015-08-08 18:10:22 -0400

    Initialization for Project and Backer

---
+++ lib/backer.rb
@@ -0,0 +1,13 @@
+class Backer 
+attr_accessor :name
+
+	def initialize(name)
+		@name = name
+	end
+
+	def back_project(project)
+		backed_projects = []
+		backed_projects << Project.new(project)
+	end
+	
+end
